38055daec3ac84821d35477e7289b8b54452e6c4
Layla <layla.github@ilikelayla.top>
2025-10-19 00:23:18 +0800

    Change Icons
    do some customized updates

--- layla-type_frontend/src/App.js
+++ layla-type_frontend/src/App.js
@@ -6,17 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='App-text'>
+          <p>
+            Welcome to <code>`Layla Type`</code> project and it will come very soon.
+          </p>
+        </div>
       </header>
     </div>
   );
